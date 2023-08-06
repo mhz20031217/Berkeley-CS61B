9dcd49afe02f3a8b7f609617eff24180d9c13cc4
Hongzhi Mao <mhz20031217@126.com>
2023-08-06 14:12:47 +0800

    Fixed quicksort.

--- lab12/QuickSort.java
+++ lab12/QuickSort.java
@@ -1,4 +1,7 @@
 import edu.princeton.cs.algs4.Queue;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 public class QuickSort {
     /**
@@ -39,19 +42,23 @@ public class QuickSort {
      * @param pivot     The item to pivot on
      * @param lt      An empty Queue. When the function completes, this queue will contain
      *                  all of the items in unsorted that are less than the given pivot.
-     * @param ge   An empty Queue. When the function completes, this queue will contain
+     * @param eq   An empty Queue. When the function completes, this queue will contain
+     *                  all of the items in unsorted that are equal to the given pivot.
+     * @param gt   An empty Queue. When the function completes, this queue will contain
      *                  all of the items in unsorted that are greater than the given pivot.
      */
     private static <Item extends Comparable> void partition(
             Queue<Item> unsorted, Item pivot,
-            Queue<Item> lt, Queue<Item> ge) {
+            Queue<Item> lt, Queue<Item> eq, Queue<Item> gt) {
         while (!unsorted.isEmpty()) {
             Item item = unsorted.dequeue();
             int cmp = item.compareTo(pivot);
             if (cmp < 0) {
                 lt.enqueue(item);
+            } else if (cmp > 0) {
+                gt.enqueue(item);
             } else {
-                ge.enqueue(item);
+                eq.enqueue(item);
             }
         }
     }
@@ -63,13 +70,58 @@ public class QuickSort {
             return items;
         }
         Item pivot = getRandomItem(items);
-        Queue<Item> lt, ge;
+        Queue<Item> lt, eq, gt;
         lt = new Queue<>();
-        ge = new Queue<>();
-        partition(items, pivot, lt, ge);
+        eq = new Queue<>();
+        gt = new Queue<>();
+        partition(items, pivot, lt, eq, gt);
         lt = quickSort(lt);
-        ge = quickSort(ge);
-        items = catenate(lt, ge);
+        gt = quickSort(gt);
+        items = catenate(lt, catenate(eq, gt));
         return items;
+    }
+
+    @Test
+    public void testQuickSort1() {
+        Integer[] orin = {9, 1, 3, 5, 6, 8, 7, 4, 2};
+        Integer[] ans = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        Queue<Integer> queue = new Queue<>();
+        for (Integer i : orin) {
+            queue.enqueue(i);
+        }
+        Queue<Integer> sorted = quickSort(queue);
+        for (int i = 0; i < 9; i++) {
+            orin[i] = sorted.dequeue();
+        }
+        boolean flag = true;
+        for (int i = 0; i < 9; i++) {
+            if (!orin[i].equals(ans[i])) {
+                flag = false;
+                break;
+            }
+        }
+        assertTrue(flag);
+    }
+
+    @Test
+    public void testQuickSort2() {
+        Integer[] orin = {4, 6, 1, 4, 7, 9, 1, 6, 0, 1};
+        Integer[] ans = {0, 1, 1, 1, 4, 4, 6, 6, 7, 9};
+        Queue<Integer> queue = new Queue<>();
+        for (Integer i : orin) {
+            queue.enqueue(i);
+        }
+        Queue<Integer> sorted = quickSort(queue);
+        for (int i = 0; i < 10; i++) {
+            orin[i] = sorted.dequeue();
+        }
+        boolean flag = true;
+        for (int i = 0; i < 10; i++) {
+            if (!orin[i].equals(ans[i])) {
+                flag = false;
+                break;
+            }
+        }
+        assertTrue(flag);
     }
 }
